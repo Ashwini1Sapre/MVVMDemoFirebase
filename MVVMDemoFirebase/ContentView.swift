d5b1897bc635f94699f4f5f9b9af38ec79e5c66f
knoxpomacbookpro <himanshu.gandhi.knoxpo@gmail.com>
2021-01-06 11:13:29 +0530

    Initial Commit

---
+++ MVVMDemoFirebase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MVVMDemoFirebase
+//
+//  Created by Knoxpo MacBook Pro on 06/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
